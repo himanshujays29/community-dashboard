8058ffdbfc147cb4ea69888ba2fd311b5ccc53c2
Me Priyank <pvonlyone29@gmail.com>
2026-01-02 20:16:27 +0530

    unnecessary changes removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,8 +27,8 @@ export const metadata: Metadata = {
   description: config.meta.description,
   icons: {
     icon: config.meta.favicon_url,
-    shortcut: '../public/favicon.ico',
-    apple: '../public/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/favicon.ico',
   },
   openGraph: {
     title: config.meta.title,
